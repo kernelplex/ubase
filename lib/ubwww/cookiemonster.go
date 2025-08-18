55235399d374205ce5a4cdc27fc24454cfe4b823
Shawn Eddy <ymiseddy@gmail.com>
2025-08-18 18:09:44 -0400

    feat: add CookieMonster for auth token cookie management
    Implement CookieMonster package for handling authentication token cookies with encryption, expiration, and middleware support. Includes methods for reading, writing, clearing cookies, and middleware integration.

---
+++ lib/ubwww/cookiemonster.go
@@ -0,0 +1,170 @@
+package ubwww
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/kernelplex/ubase/lib/ubsecurity"
+)
+
+type CookieContextKey string
+
+type AuthTokenCookie interface {
+	IsExpired() bool
+	Touch()
+}
+
+type AuthTokenCookieManager[T AuthTokenCookie] interface {
+	ClearAuthTokenCookie(w http.ResponseWriter)
+	ReadAuthTokenCookie(r *http.Request) (bool, T, error)
+	WriteAuthTokenCookie(w http.ResponseWriter, token T) error
+	Middleware(handler http.Handler) http.Handler
+	MiddlewareFunc(handler http.HandlerFunc) http.HandlerFunc
+	FromContext(ctx context.Context) (T, bool)
+}
+
+type CookieMonster[T AuthTokenCookie] struct {
+	encryptionService ubsecurity.EncryptionService
+	cookieName        string
+	tokenSoftExpiry   int
+	secure            bool
+	cookieKey         CookieContextKey
+}
+
+func NewCookieMonster[T AuthTokenCookie](
+	encryptionService ubsecurity.EncryptionService,
+	cookieName string,
+	secure bool,
+	tokenSoftExpiry int64,
+	cookieKey CookieContextKey) AuthTokenCookieManager[T] {
+	return &CookieMonster[T]{
+		encryptionService: encryptionService,
+		cookieName:        cookieName,
+		secure:            secure,
+		tokenSoftExpiry:   int(tokenSoftExpiry),
+		cookieKey:         cookieKey,
+	}
+}
+
+func (c *CookieMonster[T]) ClearAuthTokenCookie(w http.ResponseWriter) {
+	cookie := http.Cookie{
+		Name:     c.cookieName,
+		Value:    "",
+		HttpOnly: true,
+		Secure:   c.secure,
+		SameSite: http.SameSiteStrictMode,
+		MaxAge:   -1,
+		Path:     "/",
+	}
+
+	slog.Debug("Writing auth token cookie", "cookie", cookie)
+
+	http.SetCookie(w, &cookie)
+}
+
+func (c *CookieMonster[T]) ReadAuthTokenCookie(r *http.Request) (bool, T, error) {
+	slog.Debug("Reading auth token cookie")
+	var zero T
+	cookie, err := r.Cookie(c.cookieName)
+	if err != nil {
+		slog.Debug("Error reading auth token cookie", "error", err)
+		return false, zero, err
+	}
+	slog.Debug("Auth token cookie found", "cookie", cookie)
+	tokenBytes, err := c.encryptionService.Decrypt64(cookie.Value)
+	if err != nil {
+		slog.Debug("Error decrypting auth token cookie", "error", err)
+		return false, zero, fmt.Errorf("failed to decrypt auth token cookie: %w", err)
+	}
+	slog.Debug("Decrypted auth token cookie", "cookie", cookie)
+
+	var jsonToken T
+	err = json.Unmarshal(tokenBytes, &jsonToken)
+	if err != nil {
+		slog.Debug("Error unmarshaling auth token cookie", "error", err)
+		return false, zero, fmt.Errorf("failed to unmarshal auth token cookie: %w", err)
+	}
+	slog.Debug("Auth token cookie unmarshaled", "token", jsonToken)
+	return true, jsonToken, nil
+}
+
+func (c *CookieMonster[T]) WriteAuthTokenCookie(w http.ResponseWriter, token T) error {
+	tokenJson, err := json.Marshal(token)
+	if err != nil {
+		return err
+	}
+
+	encryptedToken, err := c.encryptionService.Encrypt64(string(tokenJson))
+	if err != nil {
+		return err
+	}
+
+	cookie := http.Cookie{
+		Name:     c.cookieName,
+		Value:    string(encryptedToken),
+		HttpOnly: true,
+		Secure:   c.secure,
+		SameSite: http.SameSiteStrictMode,
+		MaxAge:   c.tokenSoftExpiry,
+		Path:     "/",
+	}
+
+	slog.Debug("Writing auth token cookie", "cookie", cookie)
+
+	http.SetCookie(w, &cookie)
+	return nil
+}
+
+func (c *CookieMonster[T]) FromContext(ctx context.Context) (T, bool) {
+	token, ok := ctx.Value(c.cookieKey).(T)
+	return token, ok
+}
+
+func (c *CookieMonster[T]) MiddlewareFunc(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		found, token, err := c.ReadAuthTokenCookie(r)
+
+		if err == nil && found {
+			if token.IsExpired() {
+				slog.Debug("Auth token cookie is expired, clearing cookie")
+				c.ClearAuthTokenCookie(w)
+			} else {
+				token.Touch()
+				err = c.WriteAuthTokenCookie(w, token)
+				if err != nil {
+					slog.Error("Error updating auth token cookie", "error", err)
+				}
+				ctx := r.Context()
+				ctx = context.WithValue(ctx, c.cookieKey, token)
+				r = r.WithContext(ctx)
+			}
+		}
+		handler(w, r)
+	}
+}
+
+func (c *CookieMonster[T]) Middleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		found, token, err := c.ReadAuthTokenCookie(r)
+
+		if err == nil && found {
+			if token.IsExpired() {
+				slog.Debug("Auth token cookie is expired, clearing cookie")
+				c.ClearAuthTokenCookie(w)
+			} else {
+				token.Touch()
+				err = c.WriteAuthTokenCookie(w, token)
+				if err != nil {
+					slog.Error("Error updating auth token cookie", "error", err)
+				}
+				ctx := r.Context()
+				ctx = context.WithValue(ctx, c.cookieKey, token)
+				r = r.WithContext(ctx)
+			}
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
